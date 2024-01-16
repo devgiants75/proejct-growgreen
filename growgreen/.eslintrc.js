241f030714a6006cee8b3e0090cdfc66eaed1614
ijyan <guuhwi@gmail.com>
2024-01-16 17:51:30 +0900

    프로그램 환경 세팅

---
+++ growgreen/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 14,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-unused-vars': ['warn'],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.tsx'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'global-require': 0,
+    'react/jsx-no-useless-fragment': [2, { allowExpressions: true }], // Fragment
+  },
+  ignorePatterns: ['.eslintrc.js'],
+};
