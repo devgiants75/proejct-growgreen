3e18ca780de6b53048951e56eb476a865a923e6f
chaezerojj <yyzz1219@gmail.com>
2024-01-16 22:14:16 +0900

    20240116 jcy

--- growgreen/.eslintrc.js
+++ growgreen/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     ecmaVersion: 14,
     sourceType: 'module',
     project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
@@ -38,6 +39,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'global-require': 0,
     'react/jsx-no-useless-fragment': [2, { allowExpressions: true }], // Fragment
+    "import/prefer-default-export": 0, // export default 권장
   },
   ignorePatterns: ['.eslintrc.js'],
 };
