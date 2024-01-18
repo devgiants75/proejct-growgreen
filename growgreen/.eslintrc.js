b2fb9edfeb4a19b71dc6abca5fdfff5507582c40
devgiants75 <134823635+devgiants75@users.noreply.github.com>
2024-01-18 14:48:00 +0900

    Revert "20240116 jcy"

--- growgreen/.eslintrc.js
+++ growgreen/.eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
     ecmaVersion: 14,
     sourceType: 'module',
     project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
@@ -39,7 +38,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'global-require': 0,
     'react/jsx-no-useless-fragment': [2, { allowExpressions: true }], // Fragment
-    "import/prefer-default-export": 0, // export default 권장
   },
   ignorePatterns: ['.eslintrc.js'],
 };
