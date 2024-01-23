087f2c88d6e8e6567496d07ba363084fed49e65a
kimkyeonghye <guuhwi@gmail.com>
2024-01-24 02:36:39 +0900

    kkh/Feat: footer UI 구현

--- growgreen/src/pages/home/index.tsx
+++ growgreen/src/pages/home/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 // 홈 화면 페이지
 export default function Index() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
