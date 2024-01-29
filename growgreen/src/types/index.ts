f3d736cc6fe913447d3a6b4bdc93447ab72d1b01
ijyan <guuhwi@gmail.com>
2024-01-30 02:36:41 +0900

    kkh/Feat: Card 리스트 구현

--- growgreen/src/types/index.ts
+++ growgreen/src/types/index.ts
@@ -1,2 +1,9 @@
 // TypeScript 타입 정의와 인터페이스를 보관
-export const tmp = '';
+export interface CardType {
+  id: number;
+  categoryId?: number;
+  category: string;
+  title: string;
+  thumbnail: string;
+  url: string;
+}
