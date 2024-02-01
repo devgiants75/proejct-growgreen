242caf8e662914a704c8d5af67e3f502ac4cfb79
chaezerojj <yyzz1219@gmail.com>
2024-02-01 16:04:07 +0900

    240201 jcy

--- growgreen/src/pages/myPage/Activity/commentsWritten/index.tsx
+++ growgreen/src/pages/myPage/Activity/commentsWritten/index.tsx
@@ -1,11 +1,12 @@
-import { Typography } from '@mui/material';
 import React from 'react';
+import Tabs from '../../myPageSubTab';
 
 // 작성된 댓글 페이지
 export default function Index() {
   return (
     <div>
-      <h1>작성된 댓글</h1>
+      <p>작성된 댓글</p>
+      {/* <Tabs menu={tabs} index={3} /> */}
     </div>
   );
 }
