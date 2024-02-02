febeb1f585652330248238b6a8aadf55b573cee7
snail215 <ekdms0736@naver.com>
2024-02-02 14:11:15 +0900

    20240202 kde

--- growgreen/src/components/Community/Community.Style.tsx
+++ growgreen/src/components/Community/Community.Style.tsx
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
 
 export const Community = styled.div`
-  margin: 0 auto;
-  width: 1260px;
-  height: 300px
-  background-color: #333333;
+  
 `;
--- growgreen/src/pages/community/index.tsx
+++ growgreen/src/pages/community/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SubTitle from '../../components/SubTitle/SubTitle';
 import Tab from '../../components/Tab/Tab';
-import Community from '../../components/Community/Community';
-import { width } from '@mui/system';
+import { Box, Button, TextField } from '@mui/material';
 
 export const communityLink = [
   {
@@ -29,10 +28,38 @@ function Index() {
       <SubTitle>커뮤니티</SubTitle>
       <Tab links={communityLink} index={0} />
       <Outlet />
-
-      <div>
-        <p>전체 4,204건</p>
-      </div>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          width: '100%',
+          padding: 2
+        }}
+      >
+        {/* 체크 박스 및 텍스트 */}
+        <Box>
+          {/* 전체 게시글 수 */}
+          <Box>전체 게시글 수</Box>
+          {/* 체크 박스 */}
+          <Box>체크 박스</Box>
+        </Box>
+        {/* 검색 바 & 버튼 */}
+        <Box>
+          <Box
+            sx={{
+              fontSize: '16rem'
+            }}
+          >
+            <TextField
+              id='search-input'
+              variant='outlined'
+              size='small'
+              placeholder='커뮤니티 내에서 검색'
+            />
+          </Box>
+          <Button>검색하기</Button>
+        </Box>
+      </Box>
     </>
   );
 }
