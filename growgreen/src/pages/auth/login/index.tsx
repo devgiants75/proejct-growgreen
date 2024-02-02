097446254296bc99a6def67a51abf5be0218bdd1
chaezerojj <yyzz1219@gmail.com>
2024-02-02 15:06:17 +0900

    240202 jcy

--- growgreen/src/pages/auth/login/index.tsx
+++ growgreen/src/pages/auth/login/index.tsx
@@ -1,86 +1,137 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, rgbToHex, ThemeProvider } from '@mui/material/styles';
-import styled from '@emotion/styled';
+import { Button, Container, TextField, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useStore } from '../../../stores/user.store';
 
-const Theme = createTheme();
+interface LoginResponse {
+  token: string;
+}
 
-const Btn = styled(Button)`
-  font-size: 16rem;
-`;
-const TextF = styled(TextField)`
-  font-size: 20rem;
-`;
+interface User {
+  userId: string;
+  password: string;
+}
 
-// signup (login)
-export default function Index() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
+function Index() {
+  const [tabValue, setTabValue] = useState<string>('user');
+  const [userId, setUserId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const [cookies, setCookie] = useCookies(['userToken', 'userType']);
+
+  const { setUser } = useStore();
+
+  const navigate = useNavigate();
+
+  const handleTabChange = (event: ChangeEvent<object>, newValue: string) => {
+    setTabValue(newValue);
+  };
+
+  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      // 사용자 목록을 가져옵니다.
+      const usersResponse = await axios.get('http://localhost:5000/users');
+      const users = usersResponse.data;
+
+      // 입력된 userId와 password가 일치하는 사용자를 찾습니다.
+      const user = users.find(
+        (user: User) => user.userId === userId && user.password === password,
+      );
+
+      if (user) {
+        // 임의의 토큰 생성 (실제 환경에서는 안전한 방식으로 생성 필요)
+        const token = `faketoken-${new Date().getTime()}`;
+
+        // 쿠키에 토큰과 사용자 유형 저장
+        setCookie('userToken', token, { path: '/' });
+        setCookie('userType', tabValue, { path: '/' });
+
+        setUser(user);
+
+        console.log('Login successful');
+
+        navigate('/');
+      } else {
+        // 일치하는 사용자가 없는 경우
+        console.error('Login failed: Invalid credentials');
+      }
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
   };
 
   return (
-    <ThemeProvider theme={Theme}>
+    <Box
+      sx={{
+        width: '100%',
+        mt: 30,
+      }}
+    >
+      <Typography
+        component="h1"
+        variant="h4"
+        sx={{ textAlign: 'center', mb: 10 }}
+      >
+        로그인
+      </Typography>
+      <Tabs value={tabValue} onChange={handleTabChange} centered>
+        <Tab label="회원 계정" value="user" />
+        <Tab label="관리자 계정" value="admin" />
+      </Tabs>
+
       <Container component="main" maxWidth="xs">
         <Box
           sx={{
-            mt: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
         >
-          <form onSubmit={handleSubmit} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography
-                  align="center"
-                  sx={{ mb: 1 }}
-                  style={{ fontSize: '25rem' }}
-                >
-                  로그인
-                </Typography>
-                {/* 아이디 */}
-                {/* <p style={{ fontSize: 16 }}>아이디</p> */}
-                <TextF
-                  required
-                  fullWidth
-                  id="userId"
-                  label="Outlined"
-                  variant="outlined"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                {/* 비밀번호 */}
-                {/* <p style={{ fontSize: 16 }}>비밀번호</p> */}
-                <TextF
-                  required
-                  fullWidth
-                  id="password"
-                  label="Password"
-                  type="password"
-                />
-              </Grid>
-            </Grid>
-            {/* 로그인 버튼 */}
-            <Btn
+          <Box component="form" onSubmit={handleLogin} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              value={userId}
+              onChange={e => setUserId(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
               Login
-            </Btn>
-          </form>
+            </Button>
+          </Box>
         </Box>
       </Container>
-    </ThemeProvider>
+    </Box>
   );
 }
+
+export default Index;
--- growgreen/src/pages/myPage/profile/profileSetting/index.tsx
+++ growgreen/src/pages/myPage/profile/profileSetting/index.tsx
@@ -1,11 +1,18 @@
-import { Container } from '@mui/material';
+import { Box, List } from '@mui/material';
+import { Outlet } from 'react-router-dom';
 import React from 'react';
+import Tabs from '../../myPageSubTab';
+import { myPageLinks } from '../../myPageHome';
 
-// 프로필 설정 페이지
+// 프로필 설정
 export default function Index() {
   return (
-    <Container>
-      <h2>프로필 설정</h2>
-    </Container>
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <Outlet />
+      <List component="nav" aria-label="profile tab" sx={{ ml: 3 }}>
+        <h2>프로필 설정</h2>
+      </List>
+      <Tabs menu={myPageLinks} index={0} />
+    </Box>
   );
 }
